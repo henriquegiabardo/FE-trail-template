9c34eecc0d971ba3131b552d53d84014d5606531
Henrique Geribello <henriquegiabardo@usp.br>
2020-06-24 01:43:38 -0300

    versao do board.js com os comentarios feito pós code review/ pair programing com o Freitas. codigo está mais legivel

--- src/node/board.js
+++ src/node/board.js
@@ -15,14 +15,13 @@ function createMatrix(size) {
   return matrix;
 }
 
-function viewBoard(matrix, size) {
-  const lastLine = size - 1;
-  // devo passar o size pra garantir ou eu pego o size da matrix?
-  printHeader(size);
-  for (let lineIndex = 0; lineIndex < size; lineIndex += 1) {
-    printLine(matrix, lineIndex, size, 'x', 'y');
+function viewBoard(matrix, matrixSize) {
+  const lastLine = matrixSize - 1;
+  printHeader(matrixSize);
+  for (let lineIndex = 0; lineIndex < matrixSize; lineIndex += 1) {
+    printLine(matrix[lineIndex], lineIndex, matrixSize, 'x', 'y');
     if (lineIndex !== lastLine) {
-      printIntermediateLine(size);
+      printIntermediateLine(matrixSize);
     }
   }
 }
@@ -35,33 +34,33 @@ function printHeader(size) {
   console.log('');
 }
 
-// trocar os if else if por um função print symbol
-// trocar size por matrixSize
-// trocar line por lineIndex
-// trocar o matrix[line]
-
 function printLine(
-  matrix,
+  line,
   lineIndex,
-  size,
+  matrixSize,
   firstPlayerSymbol,
   secondPlayerSymbol,
 ) {
-  const lastColumn = size - 1;
+  const lastColumn = matrixSize - 1;
   process.stdout.write(`${lineIndex} `);
-  for (let column = 0; column < size; column += 1) {
-    if (matrix[lineIndex][column] === 'Blank') {
-      process.stdout.write(`${''} `);
-    } else if (matrix[lineIndex][column] === 'First Player') {
-      process.stdout.write(`${firstPlayerSymbol} `);
-    } else if (matrix[lineIndex][column] === 'Second Player') {
-      process.stdout.write(`${secondPlayerSymbol} `);
-    }
+  for (let column = 0; column < matrixSize; column += 1) {
+    const symbol = line[column];
+    printSymbol(symbol, firstPlayerSymbol, secondPlayerSymbol);
     if (column !== lastColumn) {
       process.stdout.write(`${'|'}`);
     }
   }
   console.log('');
+}
+
+function printSymbol(symbol, firstPlayerSymbol, secondPlayerSymbol) {
+  if (symbol === 'Blank') {
+    process.stdout.write(`${''} `);
+  } else if (symbol === 'First Player') {
+    process.stdout.write(`${firstPlayerSymbol} `);
+  } else if (symbol === 'Second Player') {
+    process.stdout.write(`${secondPlayerSymbol} `);
+  }
 }
 
 function printIntermediateLine(size) {
@@ -75,6 +74,10 @@ function printIntermediateLine(size) {
   }
   console.log('');
 }
+
+const matrix = createMatrix(3);
+viewBoard(matrix, 3);
+
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -93,6 +96,3 @@ rl.on('close', function() {
   console.log('\nBYE BYE !!!');
   process.exit(0);
 });
-
-const matrix = createMatrix(3);
-viewBoard(matrix, 3);
